4cc1474d8024cfebe44b6f9c0b997b4cf4881281
Jose Granados <jgranados.iastic@gmail.com>
2023-08-17 09:33:49 -0600

    html static

---
+++ HTML/js/holamundo.js
@@ -0,0 +1,15 @@
+show = true;
+	function myFunction() {
+	 document.getElementById("demo")
+	 .innerHTML = "Contenido cambiado";
+	
+	}
+	function hideShow() {
+		if (show) {
+			document.getElementById("otro").style['display']='none';
+		} else {
+			document.getElementById("otro").style['display']='block';
+		}
+		show = !show;
+	
+	}
