b1652935a167bf37ea8c778cffb3a1f1a853ec61
Jose Granados <jgranados.iastic@gmail.com>
2023-10-02 08:20:20 -0600

    angular

---
+++ app_angular/src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { StudentFormComponent } from './student-form/student-form.component';
+import { StudentListComponent } from './student-list/student-list.component';
+
+const routes: Routes = [
+  {
+    path: "",
+    component: StudentFormComponent
+  },
+  {
+    path: "list",
+    component: StudentListComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ app_angular/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { StudentFormComponent } from './student-form/student-form.component';
+import { StudentListComponent } from './student-list/student-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    StudentFormComponent,
+    StudentListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
